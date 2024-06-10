5373c299654aa21b27bd62955d4f13dffaa77cbb
Mildred Micaela Marchan Quispe <129635729+MimiX07@users.noreply.github.com>
2024-06-10 16:55:01 -0500

    feature: se agrego la logica parael login (email y password)

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
-
+import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
 
+export class LoginComponent {
+  email:string ='';
+  password: string ='';
+
+  onSubmit(form: NgForm): void {
+    if (form.valid) {
+
+      console.log('Email:', this.email);
+      console.log('Password:', this.password);
+    }
+  }
 }
