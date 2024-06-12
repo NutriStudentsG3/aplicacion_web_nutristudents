ba507ed5e48d8a08739f14af6de36d3fd9fb8bc0
Mildred Micaela Marchan Quispe <129635729+MimiX07@users.noreply.github.com>
2024-06-12 12:25:19 -0500

    app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { LoginComponent } from './auth/login/login.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
-  { path: 'login', component: NotImplementedComponent },
+  { path: 'login', component: LoginComponent },
   {
     path: '',
     component: MainLayoutComponent,
